f924e745353087624f53a5c30d4794596371b426
RoyRavid <ravid3@my.yorku.ca>
2018-11-05 11:52:16 -0500

    setup

--- tempSens.c
+++ tempSens.c
@@ -11,7 +11,6 @@ int getTemp(char* id)
  int n;
  char buf[100];
  int temp;
-
  buf[0] = '\0';
 
  filePath[0] = '\0';
@@ -30,19 +29,26 @@ int getTemp(char* id)
  buf[n] = '\0';
 
  char* tempPtr = strstr(buf, "t="); 
- temp = atoi(tempPtr + 2); 
+ temp = atoi(tempPtr + 2);
+  
  
  fclose(readingFile);
+
  return temp;
 
 }
 
 int main(int argc, char *argv[])
 {
- 
+ int highTemp = 0;
+ int lowTemp = 100000; 
  char id[100];
  int ctemp = 0;
- 
+ char commandstr[1000];
+ char htstr[10];
+ char ltstr[10];
+ char ctstr[10];
+
  if (argc != 2)
  {
   printf("Wrong arg count\n");
@@ -53,12 +59,24 @@ strcpy(id, argv[1]);
 for(;;)
 {
  int gotTemp = getTemp(id);
+ if (gotTemp < lowTemp){lowTemp = gotTemp;}
+ if (gotTemp > highTemp){highTemp = gotTemp;}
  if ((gotTemp - ctemp) > 1000 || (ctemp - gotTemp) > 1000)
  { 
   ctemp = gotTemp;
-  printf("Temperature changed to %d\n", gotTemp); 
-  system("curl -X POST https://maker.ifttt.com/trigger/{TempChanged}/with/key/dx6eyL5uEw3ydlVK32hHSw ");
+  printf("Temperature changed to %4.1f\n", (gotTemp/1000.0)); 
+
+  sprintf(htstr, "%d", highTemp);
+  sprintf(ltstr, "%d", lowTemp);  
+  sprintf(ctstr, "%d", gotTemp);
+
+  printf("Low is %d, high is %d\n", lowTemp, highTemp);
+  sprintf(commandstr, "curl -X POST -H \"Content-Type: application/json\" -d \'{\" %s \":\"High\",\" %s \":\"Low\",\" %s \":\"Current\"}\' https://maker.ifttt.com/trigger/TempChanged/with/key/dx6eyL5uEw3ydlVK32hHSw" , ltstr, htstr, ctstr);
+  system(commandstr);
   printf("\n");
+  htstr[0] = '\0';
+  ltstr[0] = '\0';
+  ctstr[0] = '\0';
 }
  sleep(5);
  
